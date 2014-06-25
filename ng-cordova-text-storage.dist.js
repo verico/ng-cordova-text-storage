0ed2980824797994907fe18f219d229f9afe9394
Andreas <andreas.waal@verico.com>
2014-06-25 15:19:58 +0200

    Removed some unused code

--- ng-cordova-text-storage.dist.js
+++ ng-cordova-text-storage.dist.js
@@ -79,40 +79,6 @@ angular.module('com.verico.ng-cordova-text-storage')
         var _private = {};
         _private.saveFolder = 'textStorage';
 
-        function onDeviceReady() {
-            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-        }
-
-        function gotFS(fileSystem) {
-            fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
-        }
-
-
-        function gotFileWriter(writer) {
-            writer.onwriteend = function(evt) {
-                console.log("contents of file now 'some sample text'");
-                writer.truncate(11);
-                writer.onwriteend = function(evt) {
-                    console.log("contents of file now 'some sample'");
-                    writer.seek(4);
-                    writer.write(" different text");
-                    writer.onwriteend = function(evt){
-                        console.log("contents of file now 'some different text'");
-                    };
-                };
-            };
-            writer.write("some sample text");
-        }
-
-        function fail(error) {
-            console.log(error.code);
-        }
-
-        function gotFileEntry(fileEntry) {
-            fileEntry.createWriter(gotFileWriter, fail);
-        }
-
-
         _private.getWriter = function(fs,name){
             var deferred = $q.defer();
 
