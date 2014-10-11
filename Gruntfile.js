65d2c691c03b13e8ff689fb95ba948aa5bb78c2f
andwaal <a_waal@hotmail.com>
2014-10-11 13:07:25 +0200

    Cleaned up package.json and gruntfile. Updated package.json with versions. No more latest-version bs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
                 dest: 'ng-cordova-text-storage.dist.js'
             }
         },
-        ngmin: {
+        ngAnnotate: {
             all:{
                 src:['ng-cordova-text-storage.dist.js'],
                 dest: 'build/ng-cordova-text-storage.ng.js'
@@ -35,28 +35,16 @@ module.exports = function(grunt) {
                 src: 'build/ng-cordova-text-storage.ng.js',
                 dest: 'ng-cordova-text-storage.min.js'
             }
-        },
-        shell: {
-            install: {
-                options: { stdout: true },
-                command: [
-                    'npm install',
-                    'bower install'
-                ].join('&&')
-            }
         }
     });
 
     //Modules
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-ng-annotate');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-
     //Tasks
-    grunt.registerTask('install', ['shell:install']);
-    grunt.registerTask('build', ['clean','karma','concat','ngmin', 'uglify']);
-    grunt.registerTask('default', ['install', 'build']);
+    grunt.registerTask('default', ['clean','karma','concat','ngAnnotate', 'uglify']);
 };
