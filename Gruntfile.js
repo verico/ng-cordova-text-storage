0f7165a5f8c17866d37bd4cff032b79673a20540
andwaal <a_waal@hotmail.com>
2014-10-11 12:52:25 +0200

    cleaned up package and gruntfile. Removed unsused such as watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,10 +51,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-ngmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
 
     //Tasks
